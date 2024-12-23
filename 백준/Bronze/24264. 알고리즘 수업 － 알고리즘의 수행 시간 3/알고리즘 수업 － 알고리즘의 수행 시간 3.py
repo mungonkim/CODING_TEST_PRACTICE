eccdd40fd73c5e817a8d44fb7c9f8477981328bf
mungon <74732825+kim-mun-gon@users.noreply.github.com>
2024-12-23 19:57:55 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.py
@@ -0,0 +1,10 @@
+def MenOfPassion(arr, n):
+    sum = 0
+    for i in range(n):
+        for j in range(n):
+            sum = sum + arr[i] * arr[j]
+    return sum
+
+n = int(input())
+print(n*n)  
+print(2)  
