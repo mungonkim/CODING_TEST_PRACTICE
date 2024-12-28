457b84b08ba3651f4da132c9fa4dea30426dbffc
mungon <74732825+kim-mun-gon@users.noreply.github.com>
2024-12-28 14:51:48 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+for _ in range(n):
+    m = int(input())
+
+    print(m // 25, end = " ")
+    m = m % 25
+    print(m // 10, end = " ")
+    m = m % 10
+    print(m // 5, end = " ")
+    m = m % 5
+    print(m)
