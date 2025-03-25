72882b84c38756825a4825202aba1a30cd8a1cea
mungon <74732825+kim-mun-gon@users.noreply.github.com>
2025-03-25 14:29:08 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.01 ms, Memory: 9.27 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -0,0 +1,9 @@
+def solution(common):
+    answer = 0
+    if common[0] + common[2] == 2*common[1]:
+        d = common[1] - common[0]
+        answer = common[-1]+d
+    else:
+        r = common[1] / common[0]
+        answer = int(common[-1] * r)
+    return answer
