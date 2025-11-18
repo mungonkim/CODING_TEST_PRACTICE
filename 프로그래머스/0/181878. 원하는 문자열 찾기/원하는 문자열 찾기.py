3d4f70e6f24bd69c3cdfbe588688c7b0a681df86
mungon <74732825+mungonkim@users.noreply.github.com>
2025-11-18 11:29:15 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 12.02 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
@@ -0,0 +1,8 @@
+def solution(myString, pat):
+    myString = myString.lower()
+    pat = pat.lower()
+    
+    for i in range(len(myString) - len(pat) + 1):
+        if myString[i:i+len(pat)] == pat:
+            return 1
+    return 0
