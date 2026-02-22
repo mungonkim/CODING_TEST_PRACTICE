7cb0e8eb37072c11e84e3c26f04b73ec02620147
mungon <74732825+mungonkim@users.noreply.github.com>
2026-02-22 17:25:38 +0900

    [Bronze III] Title: 공, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1547. 공/공.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+m = int(input())
+cup = [1, 0, 0]
+
+for _ in range(m):
+    x, y = map(int, input().split())
+    cup[x-1], cup[y-1] = cup[y-1], cup[x-1] 
+
+print(cup.index(1)+1)
