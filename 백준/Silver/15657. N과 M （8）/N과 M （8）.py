1b4a027867c03f916342a4292b3b391532627506
mungon <74732825+mungonkim@users.noreply.github.com>
2026-04-02 10:47:00 +0900

    [Silver III] Title: N과 M (8), Time: 52 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+n_list = list(map(int, input().split()))
+n_list.sort()
+result = []
+
+def dfs(start, depth):
+    if depth == M:
+        print(*result)
+        return
+    
+    for i in range(start, N):
+        result.append(n_list[i])
+        dfs(i, depth+1)
+        result.pop()
+
+dfs(0, 0)
