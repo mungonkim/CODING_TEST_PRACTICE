517d8852925c2ec57a3a72d0f4de12f5cffcb60c
mungon <74732825+mungonkim@users.noreply.github.com>
2026-07-10 09:59:32 +0900

    [Passed] 두 개의 격자 비교하기 (46ms, 16MB)

---
+++ trail0/두 개의 격자 비교하기/compare-two-grid.py
@@ -0,0 +1,12 @@
+N, M = map(int, input().split())
+
+matrix_1 = [list(map(int, input().split())) for _ in range(N)]
+matrix_2 = [list(map(int, input().split())) for _ in range(N)]
+
+for i in range(N):
+    for j in range(M):
+        if matrix_1[i][j] == matrix_2[i][j]:
+            print(0, end=' ')
+        else:
+            print(1, end=' ')
+    print()
